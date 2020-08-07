e96b702393aec8659f5bf184203e7694b4626542
Gabriel Lima <gabriel1997lima@gmail.com>
2020-08-06 22:27:38 -0300

    Add files via upload

---
+++ match.php
@@ -0,0 +1,42 @@
+{
+  "matchid": "example_match",
+  "num_maps": 1,
+  "players_per_team": 1,
+  "min_players_to_ready": 1,
+  "min_spectators_to_ready": 0,
+  "skip_veto": false,
+  "veto_first": "team1",
+  "side_type": "standard",
+  "maplist": [
+    "de_cache",
+    "de_dust2",
+    "de_inferno",
+    "de_mirage",
+    "de_nuke",
+    "de_overpass",
+    "de_train"
+  ],
+  "favored_percentage_team1": 65,
+  "favored_percentage_text": "HLTV Bets",
+  "team1": {
+    "name": "Teste2",
+    "tag": "CORNO",
+    "flag": "FR",
+    "logo": "nv",
+    "players": {
+      "STEAM_0:0:60640641": "george",
+    }
+  },
+  "team2": {
+    "name": "Teste1",
+    "tag": "KORN",
+    "flag": "SE",
+    "logo": "fntc",
+    "players": {
+      "STEAM_0:0:52631481": "gLima"
+    }
+  },
+  "cvars": {
+    "hostname": "Match server #1"
+  }
+}
