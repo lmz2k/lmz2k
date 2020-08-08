9d3014c71c1fa63c2b5d23f07eac6e275c484a96
Gabriel Lima <gabriel1997lima@gmail.com>
2020-08-07 21:16:34 -0300

    Update match.php

--- match.php
+++ match.php
@@ -4,7 +4,7 @@ echo <<<EOF
 {
   "matchid": "example_match",
   "num_maps": 1,
-  "players_per_team": 1,
+  "players_per_team": 2,
   "min_players_to_ready": 1,
   "min_spectators_to_ready": 0,
   "skip_veto": false,
@@ -22,28 +22,27 @@ echo <<<EOF
   "favored_percentage_team1": 65,
   "favored_percentage_text": "HLTV Bets",
   "team1": {
-    "name": "Teste2",
-    "tag": "CORNO",
+    "name": "EnvyUs",
+    "tag": "EnvyUs",
     "flag": "FR",
     "logo": "nv",
     "players": {
       "STEAM_0:0:60640641": "george",
-      "STEAM_0:1:104173761" : "praxedes",
-      "STEAM_0:0:8014599" : "SpiderManBR"
+      "STEAM_0:0:22622516": "netinho"
     }
   },
   "team2": {
-    "name": "Teste1",
-    "tag": "KORN",
+    "name": "Fnatic",
+    "tag": "Fnatic",
     "flag": "SE",
     "logo": "fntc",
     "players": {
-      "STEAM_0:0:52631481": "gLima",
-      "STEAM_0:1:25098971" "dudu",
+      "STEAM_0:0:181588761": "do_mato",
+      "STEAM_0:0:8014599": "SpiderManBR"
     }
   },
   "cvars": {
-    "hostname": "FUNCIONOU KRAI #1"
+    "hostname": "Match server #1"
   }
 }
 EOF;
